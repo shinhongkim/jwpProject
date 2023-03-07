7152ceb291a2efa69101cffeac732e8037122397
shinhong <nokas@naver.com>
2023-03-07 18:55:18 +0900

    Initial commit

---
+++ src/test/java/com/example/jwpproject/JwpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
